98131ba91fe6de76e8b65c9922b871959feb750a
gayu9006 <gayatribodake9060@gmail.com>
2022-03-28 20:45:14 +0530

    added changes regarding notification page

--- src/app/admin/dashboard/notification/notification.component.ts
+++ src/app/admin/dashboard/notification/notification.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.sass']
+  styleUrls: ['./notification.component.scss']
 })
 export class NotificationComponent implements OnInit {
 
